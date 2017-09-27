3033435df3a00cc262fa9ce1e326f4656f897b7c
tremo1o <cagan.sit@gmail.com>
2017-09-27 17:22:52 +0300

    Added files

---
+++ src/containers/Header.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { 
+  Collapse, 
+  Navbar, 
+  NavbarToggler, 
+  NavbarBrand, 
+  Nav, 
+  NavItem,   
+} 
+from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+    constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+       <div className="container">
+          <Navbar color="faded" className="main-header" light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link to="/home">Home</Link>
+              </NavItem>
+              <NavItem>
+              <Link to="/account">Account</Link>
+              </NavItem>
+              <NavItem>
+              <Link to="/payment">Payment</Link>
+              </NavItem>
+              <NavItem>
+              <Link to="/settings">Settings</Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+       </div>
+    );
+  }
+}
+
+export default Header;
